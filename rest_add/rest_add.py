c0c61919f6edbbc3563038c1b07bb59b7205c140
Jose Bozo <josebozo@hotmail.com>
2017-02-16 20:56:12 +0000

    Update rest_add.py

--- rest_add/rest_add.py
+++ rest_add/rest_add.py
@@ -3,7 +3,7 @@
 import redis
 from flask import Flask, jsonify, abort, request
 
-redis_server = redis.Redis('localhost')
+redis_server = redis.Redis('redis.db')
 
 app = Flask(__name__)
 
@@ -22,4 +22,4 @@ def create_key():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", debug=True)
